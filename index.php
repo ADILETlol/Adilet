b577ae169ab59108c2eae1eb51aee4a02c782633
ADILETlol <93514204+ADILETlol@users.noreply.github.com>
2021-11-01 01:56:33 -0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <p style="color:red;">I am red</p>
 <p style="color:blue;">I am blue</p>
 <p style="font-size:50px;">I am big</p>
-<img src="img_girl.jpg" alt="Girl in a jacket" width="500" height="600">
+<img src="1481789.jpg" alt="Girl in a jacket" width="500" height="600">
 </body>
 </html>
 
