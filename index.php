f01e848aaf0ef74faa3f3d8c72dcde6a62b80b47
ADILETlol <93514204+ADILETlol@users.noreply.github.com>
2021-11-19 16:11:04 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css"> 
-    <title>AdiHub</title>
-</head>
-    <style>
-        body{
-            backgroung-color:black;
-            {
-    </style>
-    <body>
-    <p>WELCOME TO MY SITE</p>
-    <a class="sign_link" href="signhub.html">sign up</a>
-    <div class="links">
-        <a href="">instagram</a>|
-        <a href="https://discord.com/channels/@me">discord</a>|
-        <a href="https://steamcommunity.com/id/pandakrot1337/">steam</a>
-    </div>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="https://img.favpng.com/10/0/3/anime-icon-away-icon-face-icon-png-favpng-kQqBnHR7jN8TxGRq3TJhqzewe.jpg">
+    <link href='http://fonts.googleapis.com/css?family=Philosopher&subset=cyrillic' rel='stylesheet' type='text/css'>
+    
+      <title>adi</title>
+      
+  </head>
+<html>
+<body>
+<h2>Log in</h2>
+  <input type="text" name="username" id="username">
+  <input type="password" name="password" id="password">
+  <input type="submit" value="Submit" onclick="login()">
+<script>
+  function login(){
+    console.log("Submit")
+    username = document.getElementById("username");
+    password = document.getElementById('password');
+    if(username.value == "adi" && password.value == "1"){
+      console.log("Correct username and password");
+      window.location.replace("portfolio.html");
+    }
+  }
+</script>
 </body>
 </html>
